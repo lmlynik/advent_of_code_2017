8fe9df8824a5e784e998cacfca708adbaeb04469
Łukasz Młynik <lmlynik@users.noreply.github.com>
2017-12-05 12:03:52 +0100

    Update advent_of_code_5.ex
    Removed line

--- lib/advent_of_code_5.ex
+++ lib/advent_of_code_5.ex
@@ -4,8 +4,7 @@ defmodule AdventOfCode5 do
   Start at the first instruction in the list. The goal is to follow the jumps until one leads outside the list.
 
   In addition, these instructions are a little strange; after each jump, the offset of that instruction increases by 1.
-   So, if you come across an offset of 3, you would move three instructions forward, but change it to a 4 for the next time it is encountered.
-
+  So, if you come across an offset of 3, you would move three instructions forward, but change it to a 4 for the next time it is encountered.
 
   """
 
